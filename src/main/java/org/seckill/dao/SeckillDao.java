4abb0fce0234fd369fa80cde4d6346b9471fe91f
liangjun <julian208@163.com>
2016-08-07 09:45:34 +0800

    秒杀实体对象类

--- src/main/java/org/seckill/dao/SeckillDao.java
+++ src/main/java/org/seckill/dao/SeckillDao.java
@@ -1,5 +1,9 @@
 package org.seckill.dao;
 
+import java.util.Date;
+
+import org.seckill.entity.Seckill;
+
 /**
  * 
  * @ClassName: SeckillDao 
@@ -9,5 +13,34 @@ package org.seckill.dao;
  *
  */
 public interface SeckillDao {
+	
+	/**
+	 * @Title: reduceNumber 
+	 * @Description: 减库存 
+	 * @param @param seckillId
+	 * @param @param killTime
+	 * @param @return    设定文件 
+	 * @return int    返回类型 
+	 */
+	int reduceNumber(long seckillId, Date killTime);
+	
+	/**
+	 * @Title: queryById 
+	 * @Description: 根据id查询秒杀对象
+	 * @param @param seckillId
+	 * @param @return    设定文件 
+	 * @return Seckill    返回类型 
+	 */
+	Seckill queryById(long seckillId);
+	
+	/**
+	 * @Title: queryAll 
+	 * @Description: 根据偏移量查询秒杀商品列表 
+	 * @param @param offet
+	 * @param @param limit
+	 * @param @return    设定文件 
+	 * @return Seckill    返回类型 
+	 */
+	<List>Seckill queryAll(int offet, int limit);
 
 }
